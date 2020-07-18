da5ddcaa9e8e9b2092515f2e4657d8f9433e7212
LeonPatmore <leon.patmore@vonage.com>
2020-07-18 22:31:18 +0100

    Fix: Disable mutation tests.

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,11 +1,6 @@
 module.exports = function(config) {
     config.set({
-        mutate: [
-            'src/**/*.js',
-            '!src/cluster/leader/spammer-leader.js',
-            '!src/cluster/follower/spammer-follower.js',
-            '!src/test/**/*',
-        ],
+        mutate: ['src/**/*.js', '!src/test/**/*'],
         mutator: 'javascript',
         packageManager: 'npm',
         reporters: ['html'],
