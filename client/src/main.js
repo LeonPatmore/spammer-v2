5720968d67bc46c8aca1a371116485727ccbbd90
LeonPatmore <leon.patmore@vonage.com>
2020-08-13 22:15:14 +0100

    Adding routes and fixing eslint prettier.

--- client/src/main.js
+++ client/src/main.js
@@ -1,22 +1,10 @@
-import Vue from 'vue'
+import Vue from 'vue';
 // import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-
-
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
-
-import myButton from './button.vue'
-
+import myButton from './button.vue';
 
 new Vue({
-  render: h => h(myButton)
-}).$mount('#app')
-
-
-// console.log(vs)
-
-
+    render: h => h(myButton),
+}).$mount('#app');
